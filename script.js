7d20132062ecec7818a7caca7948ecb8109ce37d
talle5 <talles.lima@alu.ufc.br>
2024-02-26 18:10:32 -0300

    retrabalho nas açoes de cript/decript

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-function toCript() {
-    let str = document.getElementById("text_input").value;
-    if (str != "") {
-        document.getElementById("text_input").value = "";
-        document.getElementById("output_background").style.display = "none";
-        document.getElementById("clean").style.display = "block";
-        genElement(encript(str));
-    }
-}
+const encriptButton = document.getElementById("cript_button");
+const decriptButton = document.getElementById("decript_button");
+const textStream = document.getElementById("text_input");
+const outputBackground = document.getElementById("output_background");
+const cleanButton = document.getElementById("clean");
 
-function toDecript() {
-    let str = document.getElementById("text_input").value;
+const decode = (action) => {
+    let str = textStream.value;
     if (str != "") {
-        document.getElementById("text_input").value = "";
-        document.getElementById("output_background").style.display = "none";
-        document.getElementById("clean").style.display = "block";
-        genElement(decript(str));
+        textStream.value = "";
+        outputBackground.style.display = "none";
+        cleanButton.style.display = "block";
+        genElement(action(str));
     }
-}
+};
+
+encriptButton.onclick = () => decode(encript);
+decriptButton.onclick = () => decode(decript);
+cleanButton.onclick = () => {
+    document.querySelectorAll(".messages").forEach((list) => list.remove());
+    outputBackground.style.display = "flex";
+    cleanButton.style.display = "none";
+};
 
 function encript(str) {
     let arr = [];
@@ -51,19 +55,19 @@ function decript(str) {
         arr.push(str[i]);
         switch (str[i]) {
             case "e":
-                if (str.substring(i,i+5) == "enter") {  i += 4; }
+                if (str.substring(i, i + 5) == "enter") { i += 4; }
                 break;
             case "a":
-                if (str.substring(i,i+2) == "ai") {  i += 1; }
+                if (str.substring(i, i + 2) == "ai") { i += 1; }
                 break;
             case "o":
-                if (str.substring(i,i+4) == "ober") {  i += 3; }
+                if (str.substring(i, i + 4) == "ober") { i += 3; }
                 break;
             case "u":
-                if (str.substring(i,i+4) == "ufat" ) {  i += 3; }
+                if (str.substring(i, i + 4) == "ufat") { i += 3; }
                 break;
             case "i":
-                if (str.substring(i,i+4) == "imes") {  i += 3; }
+                if (str.substring(i, i + 4) == "imes") { i += 3; }
                 break;
         }
     }
@@ -84,15 +88,9 @@ function genElement(str) {
     button.onclick = () => navigator.clipboard.writeText(str);
     button.appendChild(img);
 
-    tag.class = "messages";
+    tag.setAttribute("class", "messages");
     tag.appendChild(text);
     tag.appendChild(button);
 
     document.getElementById("list").appendChild(tag);
-}
-
-function clean() {
-    document.querySelectorAll(".messages").forEach((list) => list.remove());
-    document.getElementById("output_background").style.display = "flex";
-    document.getElementById("clean").style.display = "none";
 }
